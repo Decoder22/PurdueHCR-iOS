e2cd8048f995f9d6ae01ce4ed0204b10ab2a3f26
Decoder22 <brianjohncox232@gmail.com>
2019-02-26 00:34:00 -0500

    Fix cell updating issue
    Add a hotfix to refresh the PointLog before the Cell draws it

--- PurdueHCR/ViewControllers/PointApprovalViewControllers/PointsSubmittedViewController.swift
+++ PurdueHCR/ViewControllers/PointApprovalViewControllers/PointsSubmittedViewController.swift
@@ -69,7 +69,6 @@ class PointsSubmittedViewController: RHPApprovalTableViewController, UISearchRes
 	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ResolvedCell
-		
 		cell.activeView.layer.cornerRadius = cell.activeView.frame.width / 2
 		if (displayedLogs[indexPath.row].wasRejected() == true) {
 			cell.activeView.backgroundColor = UIColor.red
@@ -94,7 +93,6 @@ class PointsSubmittedViewController: RHPApprovalTableViewController, UISearchRes
 		let logs = self.displayedLogs[indexPath.row]
 		if ((logs.wasHandled && logs.wasRejected()) || (!logs.wasHandled && !logs.wasRejected())) {
 			let approveAction = UIContextualAction(style: .normal, title:  "Approve", handler: { (ac:UIContextualAction, view:UIView, success:(Bool) -> Void) in
-				print("Approve button tapped")
 				let log = self.displayedLogs[indexPath.row]
 				self.updatePointLogStatus(log: log, approve: true, updating: true, indexPath: indexPath)
 				if(self.displayedLogs.count == 0){
@@ -119,7 +117,6 @@ class PointsSubmittedViewController: RHPApprovalTableViewController, UISearchRes
 		let logs = self.displayedLogs[indexPath.row]
 		if (!logs.wasRejected()) {
 			let rejectAction = UIContextualAction(style: .normal, title:  "Reject", handler: { (ac:UIContextualAction, view:UIView, success:(Bool) -> Void) in
-				print("Delete button tapped")
 				let log = self.displayedLogs[indexPath.row]
 				self.updatePointLogStatus(log: log, approve: false, updating: true, indexPath: indexPath)
 				if(self.displayedLogs.count == 0){
@@ -177,16 +174,36 @@ class PointsSubmittedViewController: RHPApprovalTableViewController, UISearchRes
 				if(approve){
 					self.notify(title: "Success", subtitle: "Point approved", style: .success)
 					DispatchQueue.main.async {
+                        
+                        //This is a work around because sometimes when transistioning back from PointLogOverviewViewController, the
+                        //cell would not update the value properly. Even though it should be getting set already, we are doing in again.
+                        if(self.isFiltering()){
+                            self.filteredPoints[indexPath.row].updateApprovalStatus(approved: approve)
+                        }
+                        else{
+                            self.displayedLogs[indexPath.row].updateApprovalStatus(approved: approve)
+                        }
+                        
 						self.tableView.setEditing(false, animated: true)
-						self.tableView.reloadRows(at: [indexPath], with: .automatic)
-					}
+						self.tableView.reloadRows(at: [indexPath], with: .fade)
+                    }
 				}
 				else{
 					self.notify(title: "Success", subtitle: "Point rejected", style: .success)
 					DispatchQueue.main.async {
+                        
+                        //This is a work around because sometimes when transistioning back from PointLogOverviewViewController, the
+                        //cell would not update the value properly. Even though it should be getting set already, we are doing in again.
+                        if(self.isFiltering()){
+                            self.filteredPoints[indexPath.row].updateApprovalStatus(approved: approve)
+                        }
+                        else{
+                            self.displayedLogs[indexPath.row].updateApprovalStatus(approved: approve)
+                        }
+                        
 						self.tableView.setEditing(false, animated: true)
-						self.tableView.reloadRows(at: [indexPath], with: .automatic)
-					}
+						self.tableView.reloadRows(at: [indexPath], with: .fade)
+                    }
 				}
 			}
 		})
